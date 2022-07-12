6eadb494844bf3f70576f2a4713847cdc7ad45ea
Hera <huyhoang201555@gmail.com>
2022-07-12 15:26:22 +0700

    Lab 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 class App extends React.Component {
   constructor(props) {
